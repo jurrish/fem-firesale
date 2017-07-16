38c575b59749e03e7178555a501450aaf6706009
Frank Iriarte <jurrish@gmail.com>
2017-07-15 18:18:44 -0700

    reading from file system

--- app/main.js
+++ app/main.js
@@ -1,6 +1,29 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
+//create a window for global scope.
 let mainWindow = null;
+
+//export this method so we can pull it in from the main process in our renderer
+const getFileFromUserSelection = module.exports.getFileFromUserSelection = () => {
+  //asks to open files. returns an array of paths for files they selected
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Text Files', extensions: ['txt', 'text'] },
+      { name: 'Markdown Files', extensions: ['md', 'markdown'] },
+    ]
+  });
+
+  //if user presses cancel, we get undefined(a falsey avue) for files
+  if(!files) return;
+
+  const file = files[0];
+  const content = fs.readFileSync(file).toString();
+
+  //send the data we read in to a renderer, now we setup a listener inside the renderer! (IPC renderer module!)
+  mainWindow.webContents.send('file-opened', file, content);
+};
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ show: false });
@@ -11,6 +34,7 @@ app.on('ready', () => {
     mainWindow.show();
   });
 
+  //releases window from memory when it's closed
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,8 @@
 const marked = require('marked');
+const { remote, ipcRenderer } = require('electron');
+//import everything from our main process
+//we could destructure here if we wanted to get different methods from mainProcess
+const mainProcess = remote.require('./main');
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -7,3 +11,26 @@ const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
+
+//this function takes whatever we write in our markdown editor and uses "marked" to render it to html
+const renderMarkdownToHtml = (markdown) => {
+  //grab our #html element and put this to the innerHtml property.
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+markdownView.addEventListener('keyup', (event) => {
+  renderMarkdownToHtml(event.target.value);
+});
+
+//use remote module to communicate between main and renderer
+//we can getFileFromUserSelection now, even though it's a main process!
+openFileButton.addEventListener('click', () => {
+  mainProcess.getFileFromUserSelection();
+});
+
+//whenever we get a message from the "file-opened" CHANNEL, (what was sent from our main process), do this stuff.
+//the event object is what we're working with first (from the main channel), then the file and contents!
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+});
